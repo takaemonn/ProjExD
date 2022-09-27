51c508e9e333147f8f0c035496b5e54f86642450
takayama <c0b21105a9@edu.teu.ac.jp>
2022-09-27 15:45:41 +0900

    お試しなぞなぞ

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,1 +1,22 @@
-11
+import random
+
+def shutudai(q_list):
+    ques = random.choice(q_list)
+    print('問題：'+ques["q"])
+    return ques["a"]
+
+def kaito(ans_list):
+    ans = input("答えるんだ")
+    if ans in ans_list:
+        print("正解")
+    else:
+        print("不正解")
+
+if __name__ == "__main__":
+    q_list = [
+        {"q":"Which is stronger,Thursday or Saturday?", "a":["Saturday","saturday"]},
+        {"q":"What begins with T, finishes with T, and has T in it?", "a":["Teapot", "teapot"]},
+        {"q":"Which letter of the alphabet has the most water?","a":["C", "c"]}
+        ]
+    ans_list = shutudai(q_list)
+    kaito(ans_list)
