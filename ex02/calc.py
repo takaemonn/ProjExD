910f65c4740448e567aafd8e559f229fdb995268
takayama <c0b21105a9@edu.teu.ac.jp>
2022-10-04 15:20:13 +0900

    ５まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,3 +1,5 @@
+from ast import operator
+import numbers
 import tkinter as tk
 import tkinter.messagebox as tkm
 from turtle import right
@@ -5,20 +7,25 @@ from turtle import right
 def button_click(event):
     button = event.widget
     txt = button["text"]
-    tkm.showinfo("", f"{txt}のボタンがクリックされました")
+    #tkm.showinfo("", f"{txt}のボタンがクリックされました")
+    entry.insert(tk.END, txt)
+
 
 root = tk.Tk()
 root.title("電卓")
 root.geometry("300x560")
 
 
-r, c = 0, 0
+
 
 entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
 entry.grid(row=0, column=0, columnspan=3)
 
+
 r, c = 1, 0
-for i , num in enumerate(range(9, -1, -1),1):
+numbers = list(range(9,-1,-1))
+operators = ["+"]
+for i , num in enumerate(numbers + operators,1):
     btn = tk.Button(root, text=f"{num}", font=("Times New Roman", 30), width =4, height=2)
     btn.grid(row=r, column=c)
     btn.bind("<1>", button_click)
@@ -27,6 +34,10 @@ for i , num in enumerate(range(9, -1, -1),1):
     if i%3 ==0:
         r+=1
         c = 0
+
+#btnp = tk.Button(root, text="+", font=("Times New Roman", 30), width=4, height=2)
+#btnp.grid(row=4, column=1)
+#btn.bind("<1>", button_click)
 
 
 
