6febe5694e2722cd90080c12092da24f52eb6c0e
takayama <c0b21105a9@edu.teu.ac.jp>
2022-10-04 14:20:37 +0900

    かくにん

---
+++ ex02/clic.py
@@ -0,0 +1,30 @@
+print("hello world")
+
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showwarning("警告",f"[{txt}]ボタンが押されました")
+
+root = tk.Tk()
+root.title("おためしか")
+root.geometry("500x200")
+
+
+label = tk.Label(root,
+                text="らべるを書いてみた件",
+                font=("", 30))
+label.pack()
+
+button = tk.Button(root, text="押すな")
+button.bind("<1>", button_click)
+button.pack()
+
+entry = tk.Entry(width=30)
+entry.insert(tk.END, "fugapiyo")
+entry.pack()
+
+
+root.mainloop()
