401527c2aecda85c591aa2ca87807ed0ae20b8ce
takayama <c0b21105a9@edu.teu.ac.jp>
2022-10-18 14:27:54 +0900

    ５問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
 
+def key_down(event):
+    global key
+    key = event.keysym
+    #print(key)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -11,5 +17,9 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file = "fig/0.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag = "tori")
+
+    key = ""
+
+    root.bind("<KeyPress>", key_down)
 
     root.mainloop()
