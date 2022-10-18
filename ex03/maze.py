38417ba0e85e8a000a4256f8917128b626480dfe
takayama <c0b21105a9@edu.teu.ac.jp>
2022-10-18 14:51:28 +0900

    １０問目

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import maze_maker as mm
 
 
 def key_down(event):
@@ -45,5 +46,10 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
+
+    maze_list = mm.make_maze(15, 9)
+    print(maze_list)
+
+    mm.show_maze(canv, maze_list)
 
     root.mainloop()
