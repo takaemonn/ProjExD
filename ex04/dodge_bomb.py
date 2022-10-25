6457bbbb13976532465ed26f1e6054249e0ef12c
takayama <c0b21105a9@edu.teu.ac.jp>
2022-10-25 14:27:22 +0900

    問題 3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,15 +10,25 @@ def main():
     bg_rct = bg_sfc.get_rect()
     clock = pg.time.Clock()
 
+
+    tori_sfc = pg.image.load("fig/5.png") #surface
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect() #rect
+    tori_rct.center = 900, 400
+    
+
     
 
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
-        pg.display.update()
-
+    
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        scrn_sfc.blit(tori_sfc, tori_rct)
+
+
+        pg.display.update()
         clock.tick(1000)
 
 
